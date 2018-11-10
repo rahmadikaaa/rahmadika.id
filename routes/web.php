fe51cfbb10af19f0a1324ee044450a613ace894c
rahmadikaaa <rahmadika.putera@gmail.com>
2018-11-10 17:45:55 +0700

    a bit

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('home');
+
